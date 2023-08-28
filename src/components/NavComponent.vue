c4f5535697bf24b0e66a44d05877dcbacc647905
samiraTsakeng <tsakengsamira@gmail.com>
2023-08-28 15:28:02 +0100

    first commit

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,32 @@
+<template>
+    <nav class="navbar">
+        <h2 class="navbar-logo"><a href="#">Internship App</a></h2>
+        <div class="navbar-menu ">
+             <a href="index.html">Home</a>
+            <a href="#about">About</a> 
+            <a href="#Services">Services</a>
+            <!--<a href="#index.html">Home</a>-->
+            <a href="#company">Companies</a>
+            <div class="dropdown">
+                <button class="dropbtn">Register</button>
+                <div class="dropdown-content">
+                  <a href="intern form.html">Intern</a>
+                  <a href="job_seeker_form.html">Job seeker</a>
+                  <a href="Company form.html">Company</a>
+                </div>
+              </div>
+           
+        </div>
+        <div class="Menu-toggle">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavComponent'
+}
+</script>
---
+++ src/components/PictureHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="header-title">
+      FIND YOUR<br /><span>INTERNSHIP</span><br />
+      OR JOB EASILY
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'PictureHeader'
+};
+</script>
+
